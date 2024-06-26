0fb9a79b7964e84cfd9e51992edcf37b3685c1dd
VarshiniD29 <varshinidunnala@gmail.com>
2024-06-25 23:21:05 -0400

    Experience section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import { InView } from 'react-intersection-observer';
 import styles from "./Experience.module.css";
-import skills from "../../data/skills.json";
 import history from "../../data/history.json";
-import { getImageUrl } from "../../utlis"
+import skills from "../../data/skills.json";
+import { getImageUrl } from "../../utlis";
 
 export const Experience = () => {
   return (
@@ -11,37 +11,37 @@ export const Experience = () => {
       <h2 className={styles.title}>Experience</h2>
       <div className={styles.content}>
         <div className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+          {skills.map((skill, id) => (
+            <InView key={id} triggerOnce={false}>
+              {({ inView, ref }) => (
+                <div ref={ref} className={`${styles.skill} ${inView ? styles.animate : ''}`}>
+                  <div className={styles.skillImageContainer}>
+                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                  </div>
+                  <p>{skill.title}</p>
                 </div>
-                <p>{skill.title}</p>
-              </div>
-            );
-          })}
+              )}
+            </InView>
+          ))}
         </div>
         <ul className={styles.history}>
-          {history.map((historyItem, id) => {
-            return (
-              <li key={id} className={styles.historyItem}>
-                <img
-                  src={getImageUrl(historyItem.imageSrc)}
-                  alt={`${historyItem.organisation} Logo`}
-                />
-                <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experience, id) => {
-                      return <li key={id}>{experience}</li>;
-                    })}
-                  </ul>
-                </div>
-              </li>
-            );
-          })}
+          {history.map((historyItem, id) => (
+            <InView key={id} triggerOnce={false}>
+              {({ inView, ref }) => (
+                <li ref={ref} className={`${styles.historyItem} ${inView ? styles.animate : ''}`}>
+                  <div className={styles.historyItemDetails}>
+                    <h3>{historyItem.role}</h3>
+                    <p>{historyItem.organisation}</p>
+                    <ul>
+                      {historyItem.experiences.map((experience, id) => (
+                        <li key={id}>{experience}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
+              )}
+            </InView>
+          ))}
         </ul>
       </div>
     </section>
